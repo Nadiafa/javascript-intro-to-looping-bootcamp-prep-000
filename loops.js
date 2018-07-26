2bd38a06a8b899279a8174b7f5ee242924a8953f
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 15:12:08 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,6 @@
+for (var i=0; i>=25; i++) {
+  console.log(`"I am ${i} strange loop ${i === 0 ? '' : 's'}."`)
+}
+
+
+//forLoop(array) adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."`to an array 25 times
