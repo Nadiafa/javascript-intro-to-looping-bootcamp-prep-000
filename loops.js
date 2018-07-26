60fe2f98b3cac364238a2c5b198f70fed7735c90
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 15:14:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,10 @@
 for (var i=0; i>25; i++) {
-  console.log(`"I am ${i} strange loop ${i === 0 ? '' : 's'}."`)
+  if (i===1){
+    console.log(`"I am ${i} strange loop."`)
+  } else {
+  console.log(`"I am ${i} strange loops."`)
+    
+  }
 }
 
 
