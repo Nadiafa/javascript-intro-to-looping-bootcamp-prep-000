ccf657d8aba07e239c49d3d073c2ec2d1dd53310
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 15:24:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,18 +11,8 @@ function forLoop(array) {
 }
 
 
-
-// function forLoop(array) {
-//   for (let i = 0; i < 25; i++) {
-//     if (i === 1) {
-//       array.push("I am 1 strange loop.")
-//     } else {
-//       array.push(`I am ${i} strange loops.`)
-//     }
-//   }
-
-//   return array
-// }
-
-
-//forLoop(array) adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."`to an array 25 times
+function whileLoop(n) {
+  while (var i=n.length-1; i=0; i--) {
+    console.log(i);
+  }
+}
